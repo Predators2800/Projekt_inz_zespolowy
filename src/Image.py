cdb835328543420a26c837c95b8764d31e942866
Remi <17963123+Re-git@users.noreply.github.com>
2022-04-10 22:44:42 +0200

    added Image class to hold all things related to images

--- src/Image.py
+++ src/Image.py
@@ -1,15 +1,31 @@
 import dearpygui.dearpygui as dpg
-class Image():
 
-    def __init__(self, path, image_data, width, height , is_selected=False, tags=[]):
+class Image():
+    
+    def __init__(self, path, image_data, width, height , is_selected=False, tags=[], category=None, texture_id=None, dpg_image_id=None):
         self.path = path
-        self.image_data = image_data
         self.width = width
         self.height = height
-        self.is_selected = is_selected
+        self.image_data = image_data
         self.tags = tags
-    def wyswietl(self):
-        dpg.add_button(callback=delete,user_data=self)
+        self.category = category
+        self.texture_id = texture_id
+        self.dpg_image_id = dpg_image_id
+        self.is_selected = is_selected
 
-def delete(obiektimage):
-    obiektimage.path
+    def __repr__(self) -> str:
+        return f"path: {self.path}\n\
+                width: {self.width}\n\
+                height: {self.height}\n\
+                tags: {self.tags}\n\
+                category: {self.category}\n\
+                texture_id: {self.texture_id}\n\
+                dpg_image_id: {self.dpg_image_id}\n\
+                is_selected: {self.is_selected}"
+
+    def show(self, parent):
+        self.dpg_image = dpg.add_image(self.texture, parrent=parent)
+        return self.dpg_image
+
+    def remove(self):
+        dpg.delete_item(self.dpg_image)
