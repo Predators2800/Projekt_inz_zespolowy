965fba247fc73cc06dfa7cdcaed4196089523668
Re-git <17963123+vejitatoja@users.noreply.github.com>
2022-04-10 18:52:40 +0200

    image class implementation draft

--- src/frontend.py
+++ src/frontend.py
@@ -6,6 +6,8 @@ main_window = dpg.add_window(label="okno")
 dpg.set_primary_window(main_window, True)
 
 from contextlib import contextmanager
+
+
 @contextmanager
 def popup(*, connect_to):
     variable = dpg.add_window(show=False)
@@ -15,28 +17,44 @@ def popup(*, connect_to):
     dpg.pop_container_stack()
 
 
+class Image():
+    def __init__(self, path, category, texture, dpg_image):
+        self.path = path
+        self.category = category
+        self.texture = texture
+        self.dpg_image = dpg_image
 
+    def __repr__(self) -> str:
+        return f"{self.path}, {self.category}"
 
+    def show(self, parent):
+        self.dpg_image = dpg.add_image(self.texture, parrent=parent)
+        return self.dpg_image
 
-def fn():
-    dpg.delete_item("test")
-    with dpg.window(tag="test") as w:
-        dpg.add_text(f"okno 2, parent={w}")
+    def remove(self):
+        dpg.delete_item(self.dpg_image)
 
-
-with dpg.window(tag="test") as w:
-    dpg.add_text(f"okno 1, parent={w}")
-    dpg.add_button(label="add_window",callback=fn)
+with dpg.window():
     dpg.add_button(label="show_popup", tag="show_popup")
 
-with popup(connect_to="show_popup") as name:
+with popup(connect_to="show_popup"):
     dpg.add_text(default_value="POPUP!")
     dpg.add_button(label="added without a parrent")
     
+categories = ["cat1","cat2","cat3","cat4"]
 
+from random import choice
 
+images = {}
 
+for o in range(1_0):
+    image = Image(o,choice(categories))
+    if image.category not in images:
+        images[image.category] = [image]
+    images[image.category].append(image)
+print("finished creating objects")
 
+print(images)
 
 
 
