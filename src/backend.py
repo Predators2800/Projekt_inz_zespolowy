b81dcb68cc339950744df5954a037d4fe0668f50
Remi <17963123+Re-git@users.noreply.github.com>
2022-04-14 19:53:47 +0200

    added function for loading image in PIL format. Added function for format conversion from PIL to dpgImage. Added Predictor class for image recognition

--- src/backend.py
+++ src/backend.py
@@ -2,11 +2,46 @@ import dearpygui.dearpygui as dpg
 import pathlib
 import os
 import time
-from frontend_components import add_thumbnail_panel
 from Image import Image
+from PIL import Image as pil_image
+import numpy as np
+from imageai.Classification import ImageClassification
+
+def image_to_dpg_image(image):
+    """
+    Converst PIL image format to dpg image format
+    """
+    image.putalpha(255)
+    dpg_image = np.frombuffer(image.tobytes(), dtype=np.uint8) / 255.0
+    return dpg_image
+
+def image_load(file_path):
+    """
+    Loads image in PIL format from file path
+    Can be used by Predictor class to recognize image.
+    """
+    image = pil_image.open(file_path)
+    return image
+
+class Predictor():
+    """
+    Predictor class used for recognizing images.
+    """
+    def __init__(self):
+        self._prediction = ImageClassification()
+        self._prediction.setModelTypeAsResNet50()
+        self._prediction.setModelPath("resnet50_imagenet_tf.2.0.h5")
+        self._prediction.loadModel()
+
+    def recognize(self, file_path):
+        predictions, probabilities = self._prediction.classifyImage(file_path, input_type='array' )
+        return predictions, probabilities
 
 
 def get_file_list(folder, extensions=[".jpg",".jpeg",".png",".gif",".bmp"]):
+    """
+    Returns list of files in folder with given extensions.
+    """
     fileList = []
     for dirpath, dirnames, filenames in os.walk(folder):
         for filename in filenames:
