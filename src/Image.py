e8012ede9652e35c7f2e9623a4405e7610150f92
Szymon Winiarski <szwiniarski@gmail.com>
2022-04-22 01:03:44 +0200

    working delete function and refreshing thumbnails

--- src/Image.py
+++ src/Image.py
@@ -1,8 +1,12 @@
 import dearpygui.dearpygui as dpg
 
-class Image():
+
+class Image:
+    IMAGES = []
+    SELECTED_IMAGES = []
     
-    def __init__(self, path, image_data, width, height , is_selected=False, tags=[], category=None, texture_id=None, dpg_image_id=None):
+    def __init__(self, path, image_data, width, height, is_selected=False, tags=[],
+                 category=None, texture_id=None, dpg_image_id=None, parent=None, dpg_image=None):
         self.path = path
         self.width = width
         self.height = height
@@ -12,6 +16,8 @@ class Image():
         self.texture_id = texture_id
         self.dpg_image_id = dpg_image_id
         self.is_selected = is_selected
+        self.parent = parent
+        self.dpg_image = dpg_image
 
     def __repr__(self) -> str:
         return f"path: {self.path}\n\
@@ -25,11 +31,17 @@ class Image():
 
     def show(self, parent):
         self.dpg_image = dpg.add_image(self.texture_id, parent=parent)
+        self.parent = parent
         return self.dpg_image
 
     def remove(self):
-        dpg.delete_item(self.dpg_image)
         dpg.delete_item(self.texture_id)
+        dpg.delete_item(self.parent)
+        Image.IMAGES.remove(self)
 
     def select(self):
         self.is_selected = not self.is_selected
+        if self.is_selected:
+            Image.SELECTED_IMAGES.append(self)
+        else:
+            Image.SELECTED_IMAGES.remove(self)
